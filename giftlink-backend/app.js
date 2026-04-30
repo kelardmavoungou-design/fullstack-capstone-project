2d2331d2cdf8db6e97039dd1e8030ab96e30edbb
kelardmavoungou-design <kelardmavoungou@gmail.com>
2026-04-30 16:02:46 -0400

    Add authRoutes and register endpoint

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -28,6 +28,9 @@ const giftRoutes = require('./routes/giftRoutes');
 // Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
 const searchRoutes = require('./routes/searchRoutes');
 
+// Step 3 - Task 1: Import authRoutes
+const authRoutes = require('./routes/authRoutes');
+
 
 const pinoHttp = require('pino-http');
 const logger = require('./logger');
@@ -40,6 +43,9 @@ app.use('/api/gifts', giftRoutes);
 
 // Search API Task 2: add the searchRoutes to the server by using the app.use() method.
 app.use('/api/search', searchRoutes);
+
+// Step 3 - Task 2: Use authRoutes for the /api/auth path
+app.use('/api/auth', authRoutes);
 
 
 // Global Error Handler
